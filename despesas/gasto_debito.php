53cd6868b7c6671bf067425d66ed010ae6fd4fc9
DELL_HAP <jailton.dpaula01@gmail.com>
2020-09-24 16:14:08 -0300

    2.0.0
    Página de CRUD de despesas débito.

--- despesas/gasto_debito.php
+++ despesas/gasto_debito.php
@@ -82,6 +82,7 @@
       <div class="modal-dialog" role="document">
         <div class="modal-content">
           <div class="modal-body">
+          <img src="../imagens/delete.png" height="100px">
               <p>Deseja Excluir o(s) Iten(s) Selecionado(s)?</p>
           </div>
           <div class="modal-footer">
@@ -126,7 +127,7 @@
         </section>
         <section id="idSectionButtons">
             <div>
-            <button id="idBotaoCompras" type="button" data-toggle="modal" data-target="#modalCadastro">
+            <button type="button" data-toggle="modal" data-target="#modalCadastro" title="cadastrar despesas">
                 <svg width="1.5em" height="1.5em" viewBox="0 0 16 16" class="bi bi-bag-plus" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                   <path fill-rule="evenodd" d="M8 1a2.5 2.5 0 0 0-2.5 2.5V4h5v-.5A2.5 2.5 0 0 0 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5v9a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1V5H2z"/>
                   <path fill-rule="evenodd" d="M8 7.5a.5.5 0 0 1 .5.5v1.5H10a.5.5 0 0 1 0 1H8.5V12a.5.5 0 0 1-1 0v-1.5H6a.5.5 0 0 1 0-1h1.5V8a.5.5 0 0 1 .5-.5z"/>
@@ -134,7 +135,7 @@
             </button>
             </div>
             <div>
-            <button type="button" data-toggle="modal" data-target="#modalDelete">
+            <button type="button" data-toggle="modal" data-target="#modalDelete" title="Excluir itens">
                 <svg width="1.5em" height="1.5em" viewBox="0 0 16 16" class="bi bi-trash-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                   <path fill-rule="evenodd" d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5a.5.5 0 0 0-1 0v7a.5.5 0 0 0 1 0v-7z"/>
                 </svg>
@@ -147,7 +148,7 @@
                     <th>R$</th>
                     <th>Descrição</th>
                     <th>Data</th>
-                    <th>Detalhes</th>
+                    <th>Tipo</th>
                     <th><input type="checkbox"></th>
                 </tr>
                 <tr>
